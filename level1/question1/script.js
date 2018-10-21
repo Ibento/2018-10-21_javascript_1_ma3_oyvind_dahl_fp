0cd0e5221421aa76d2ac6ca18fecdd41e74b29f0
Øyvind Brungot Dahl <oyvind@ibento.no>
2018-10-21 19:52:54 +0200

    Finished level 1

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,49 @@
-//Use RegEx to validate form
+
+// Validate contact form on submit
+$('#contactForm').submit(function (e) {
+
+    // String for showing the result of validation
+    var validationResult = "";
+
+    // Validate name
+    var nameInput = $("#name"); 
+    
+    if (nameInput.val() === "") {
+        validationResult += "* Name is not present. Please enter your last name\n";
+    }
+    
+    // Validate lastname
+    var lastNameInput = $("#lastname"); 
+    
+    if (lastNameInput.val() === "") {
+        validationResult += "* Last name is not present. Please enter your last name\n";
+    }
+
+    // Validate Norwegian phone number
+    var phoneRegex = /\+\d{2} \d{8}$/;
+    var phoneInput = $("#phone"); 
+    
+    // test to see if phone number format is correct
+    if (!phoneRegex.test(phoneInput.val())) {
+        validationResult += "* Your phone number ("+phoneInput.val()+") format is wrong. Please enter correct number. \n";
+    }
+
+
+    // Validate e-mail address
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var emailInput = $("#email"); 
+    
+    // test to see if e-mail format is correct
+    if (!emailRegex.test(emailInput.val())) {
+        validationResult += "* Your e-mail address("+emailInput.val()+") format is wrong. Please enter correct e-mail. \n";
+    }
+
+    if (validationResult !== "") {
+        console.log(validationResult);
+    
+        e.preventDefault();
+    }
+    
+});
+
+
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,20 @@
 //Console.log out elements in JSON file
+$(document).ready(function() {
+
+    $.getJSON('author.json', function(authors) {
+
+        for (var i = 0; i <= authors.video.length-1; i++) {
+            console.log('Video #'+(i+1));
+            console.log('id: '+ authors.video[i]['id']);
+            console.log('name: '+ authors.video[i]['name']);
+            console.log('url: '+ authors.video[i]['url']);
+
+            for (var j = 0;j <=  authors.video[i]['author'].data.length -1; j++) {
+                console.log('--- Author #'+(j+1));
+                console.log('--- Name of the author: '+ authors.video[i]['author'].data[j].name_author);
+                console.log('--- Url of the author: '+ authors.video[i]['author'].data[j].url);
+                console.log('--- Type of the author: '+ authors.video[i]['author'].data[j].type);
+            } // looping through the authors of a video
+        } // looping through the videos
+    }); // getJSON
+}); // Document ready
